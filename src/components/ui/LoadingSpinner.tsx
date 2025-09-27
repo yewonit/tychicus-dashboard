94f5f20136b3cc1851e8874cd88918ab948ddd43
예원비즈 <yewonbiz@gmail.com>
2025-09-27 23:19:01 +0900

    fix: 사용하지 않는 import 제거
    - EmptyState.tsx에서 사용하지 않는 Box import 제거
    - LoadingSpinner.tsx에서 사용하지 않는 Box import 제거
    - ESLint 경고 해결

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { CircularProgress, Typography } from '@mui/material';
 import React from 'react';
 
 interface LoadingSpinnerProps {
